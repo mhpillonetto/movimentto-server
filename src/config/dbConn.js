f9cd451a8de2262a6d07b3cd04ee24d44d8927c3
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-13 22:31:55 -0300

    dotenv

--- src/config/dbConn.js
+++ src/config/dbConn.js
@@ -1,8 +1,10 @@
 const mongoose  = require('mongoose');
 
+const connection_string = process.env.DATABASE_URI || "mongodb+srv://mhpillonetto:snapcaster@cluster0.mikyteh.mongodb.net/movimentto-dev?retryWrites=true&w=majority"
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(connection_string, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
