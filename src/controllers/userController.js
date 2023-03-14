56daafd850aaff825fb010ed30d65b3d2ffbe35a
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-14 19:47:31 -0300

    edit: error handling login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const findUserByUsername = async (req, res) => {
     const foundUser = await User.findOne({username: req.headers.username});
    
     if(!foundUser) return res.status(204).json({"message":"No User found :("});
-    res.json(foundUser)
+    res.json(foundUser);
 };
 
 module.exports = { findUserByUsername }
