23b6646401f3c2f3e1e16f0929b25d6966f21950
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-14 17:39:55 -0300

    add: get user by username

---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../model/User');
+
+const findUserByUsername = async (req, res) => {
+    const foundUser = await User.findOne({username: req.headers.username});
+   
+    if(!foundUser) return res.status(204).json({"message":"No User found :("});
+    res.json(foundUser)
+};
+
+module.exports = { findUserByUsername }
