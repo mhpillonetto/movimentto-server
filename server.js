3970a1de7ca39da8cf259c1ac73ab64cd8439c20
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-15 17:14:48 -0300

    edit: jwt on /user

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use('/register', require('./src/routes/api/register'));
 app.use('/auth', require('./src/routes/api/auth'));
 app.use('/refresh', require('./src/routes/api/refresh'));
 app.use('/logout', require('./src/routes/api/logout'));
-app.use('/user',require('./src/routes/api/user'));
 
 //Everything under here will use jwt
 app.use(verifyJWT);
+app.use('/user',require('./src/routes/api/user'));
 
 mongoose.connection.once('open', ()=> {
     console.log('Connected to MongoDB');
