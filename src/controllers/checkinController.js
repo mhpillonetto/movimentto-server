1cfde0da316ebd8b5d21fc1769a56c6725bd348e
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-22 11:27:12 -0300

    add: get checked in drivers

--- src/controllers/checkinController.js
+++ src/controllers/checkinController.js
@@ -28,11 +28,12 @@ const getCheckedInDrivers = async (req, res) => {
     try {
         const filter = {
             userType: Constants.userType.motorista,
-            lastCheckIn: { $lte: TimeAgo.format(Date.now() - 3 * 60 * 60 * 1000) }
+            lastCheckIn: { $gte: Date.now() - 3 * 60 * 60 * 1000 }
         }
-        const checkedInDrivers = await User.find({filter})
+        const checkedInDrivers = await User.find(filter)
         return res.json(checkedInDrivers)
     } catch (error) {
+        console.log(error);
         throw new Error
     }
 }
