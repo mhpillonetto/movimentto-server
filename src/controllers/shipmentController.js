a1565fb4e08313c505b1515c0564d3a9308e6477
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-27 17:44:29 -0300

    add: expiration for shipments

--- src/controllers/shipmentController.js
+++ src/controllers/shipmentController.js
@@ -1,8 +1,10 @@
 const Shipment = require('../model/Shipment')
 
-const getAllShipments = async (req,res) => {
+const getAllShipments = async (req, res) => {
     try {
-        const allShipmentsList = await Shipment.find()
+        const allShipmentsList = await Shipment.find(
+            { createdAt: { $gte: Date.now() - 5 * 60 * 60 * 1000 } }
+        )
         res.status(200).json(allShipmentsList)
     } catch (error) {
         console.log(error)
@@ -10,11 +12,8 @@ const getAllShipments = async (req,res) => {
     }
 }
 
-const createShipment = async (req,res) => {
+const createShipment = async (req, res) => {
     try {
-        console.log('====================================');
-        console.log(req);
-        console.log('====================================');
         await Shipment.create(req.body)
         res.sendStatus(201)
     } catch (error) {
